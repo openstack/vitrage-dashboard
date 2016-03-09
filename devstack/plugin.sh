27428ec82574ad380233d0b81212292c3f2e3e76
Omer Etrog <omer.etrog@nokia.com>
2016-03-09 16:39:34 +0200

    remove vitragedashboardalarms from devstack
    Change-Id: I7f1f059afe1dbda9f409ddad1ce955f98353cc3e

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -6,7 +6,6 @@ VITRAGE_DASHBOARD_DIR=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
 function install_vitrage_dashboard {
     sudo pip install --upgrade ${VITRAGE_DASHBOARD_DIR}
     cp -a ${VITRAGE_DASHBOARD_DIR}/vitragedashboard/static ${DEST}/horizon/
-    cp -a ${VITRAGE_DASHBOARD_DIR}/vitragealarms/static ${DEST}/horizon/
     cp -a ${VITRAGE_DASHBOARD_DIR}/vitragedashboard/enabled/* ${DEST}/horizon/openstack_dashboard/enabled/
     cp -a ${VITRAGE_DASHBOARD_DIR}/vitrageclient/api/* ${DEST}/horizon/openstack_dashboard/api/
     python ${DEST}/horizon/manage.py compress --force
