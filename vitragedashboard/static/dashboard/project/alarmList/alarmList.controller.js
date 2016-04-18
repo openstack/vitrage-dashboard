86bf4a57ea25a41f9cce734dce4a656459f8de67
Omer Etrog <omer.etrog@nokia.com>
2016-04-18 16:50:21 +0300

    alarmlist autorefresh
    Change-Id: Ie2a08584ce74c72455c607fbb68476856d8cc952

--- vitragedashboard/static/dashboard/project/alarmList/alarmList.controller.js
+++ vitragedashboard/static/dashboard/project/alarmList/alarmList.controller.js
@@ -5,17 +5,50 @@
     .module('horizon.dashboard.project.vitrage')
     .controller('AlarmListController', AlarmListController);
 
-  AlarmListController.$inject = ['$scope', '$modal', 'vitrageTopologySrv'];
+  AlarmListController.$inject = ['$scope', '$modal', 'vitrageTopologySrv','$interval'];
 
-  function AlarmListController($scope, $modal, vitrageTopologySrv) {
+  function AlarmListController($scope, $modal, vitrageTopologySrv,$interval) {
     var alarmList = this;
     alarmList.alarms = [];
     alarmList.ialarms = [];
+    alarmList.$interval = $interval;
+    alarmList.checkboxAutoRefresh = true;
     $scope.STATIC_URL = STATIC_URL;
     alarmList.alarms = [];
-    vitrageTopologySrv.getAlarms('all').then(function(result){
-      alarmList.alarms = result.data;
-    });
+    alarmList.alarmInterval;
+
+    getData();
+    startCollectData();
+
+    function startCollectData() {
+      if (angular.isDefined(alarmList.alarmInterval)) return;
+      alarmList.alarmInterval = alarmList.$interval(getData,10000);
+    }
+
+    function stopCollectData() {
+      if (angular.isDefined(alarmList.alarmInterval)) {
+        alarmList.$interval.cancel(alarmList.alarmInterval);
+        alarmList.alarmInterval = undefined;
+      }
+    }
+    $scope.$on('$destroy',function(){
+      alarmList.stopCollectData();
+    })
+
+    alarmList.autoRefreshChanged = function(){
+      if (alarmList.checkboxAutoRefresh){
+        getData();
+        startCollectData();
+      }else{
+        stopCollectData();
+      }
+    }
+
+    function getData() {
+      vitrageTopologySrv.getAlarms('all').then(function(result){
+        alarmList.alarms = result.data;
+      });
+    }
 
     alarmList.onRcaClick = function(alarm) {
       var modalOptions = {
