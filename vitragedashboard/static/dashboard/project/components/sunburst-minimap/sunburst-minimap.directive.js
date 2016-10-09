8b468dedc3b73e8d0b0688b03d6d98696ece37c9
Alon Heller <alon.heller@nokia.com>
2016-10-09 13:48:09 +0300

    sunburst improvements
    Change-Id: Ib00f262386a777ad63093100b607a5c99714b886

--- vitragedashboard/static/dashboard/project/components/sunburst-minimap/sunburst-minimap.directive.js
+++ vitragedashboard/static/dashboard/project/components/sunburst-minimap/sunburst-minimap.directive.js
@@ -1,7 +1,13 @@
 angular.module('horizon.dashboard.project.vitrage')
   .directive('hzSunburstMinimap', [
-    function () {
+    function ($scope) {
+
+      function link(scope, element, attr, ctrl) {
+        scope.STATIC_URL = STATIC_URL;
+      }
+
       return {
+        link: link,
         restrict: 'E',
         scope: {
           selected: '='
