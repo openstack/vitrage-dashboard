170ea41ab58b68fd109f63bb26a6a232533de2ca
Eyal <eyal.bar-ilan@nokia.com>
2016-11-24 10:02:05 +0200

    fix the devstack installation to comply with standarts
    Change-Id: I686e0ff06da13c87b397aca2ed96f0905307e785

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -4,9 +4,16 @@
 VITRAGE_DASHBOARD_DIR=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
 
 function install_vitrage_dashboard {
-    sudo pip install --upgrade ${VITRAGE_DASHBOARD_DIR}
+    setup_develop ${VITRAGE_DASHBOARD_DIR}
+}
+
+function configure_vitrage_dashboard {
     cp -a ${VITRAGE_DASHBOARD_DIR}/enabled/* ${DEST}/horizon/openstack_dashboard/enabled/
-    python ${DEST}/horizon/manage.py compress --force
+}
+
+function init_vitrage_dashboard {
+    python ${DEST}/horizon/manage.py collectstatic --noinput
+    python ${DEST}/horizon//manage.py compress --force
 }
 
 # check for service enabled
@@ -19,12 +26,14 @@ if is_service_enabled vitrage-dashboard; then
 
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of service source
-        # no-op
-        :
-    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-        # Configure after the other layer 1 and 2 services have been configured
         echo_summary "Installing Vitrage Dashboard"
         install_vitrage_dashboard
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring Vitrage Dashboard"
+        configure_vitrage_dashboard
+        init_vitrage_dashboard
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the vitrage-dashboard service
